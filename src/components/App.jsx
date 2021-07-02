77de20e8cc6e3e53f26bc4080ccec833cf178417
Dylan Hoffman <jkioicefire.gmail.com>
2021-07-02 17:24:10 -0500

    updated player count based on players.js array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,13 @@ import Player from './Player.jsx';
 import players from '../init/players';
 
 function App() {
-       return (
-           <div>
-               <Header title="Scoreboard" totalPlayers={1}/>
-               {players.map( p => <Player key={p.id.toString()} name={p.name} />)}
-           </div>
-       );
+
+        return (
+            <div>
+                <Header title="Scoreboard" totalPlayers={players.length}/>
+                {players.map( p => <Player key={p.id.toString()} name={p.name} />)}
+            </div>
+        );
 }
 
 export default App;
